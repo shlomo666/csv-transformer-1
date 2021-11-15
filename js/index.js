c43a46f9d349aac220b75758b53ce31c96b27294
Shlomo666 <shlomo366@gmail.com>
2021-11-15 20:17:29 +0200

    csv-transformer-1

---
+++ js/index.js
@@ -0,0 +1,64 @@
+const ONE_MINUTES = 60 * 1000;
+const columnSeparator = '  ';
+const lineSeparator = '\n';
+
+async function logic(fileHandle) {
+  const avi = await getHTML5FileContent(fileHandle);
+
+  const groups = transform(avi);
+
+  download('res.csv', groups);
+}
+
+function transform(avi) {
+  const [firstDoc, ...restDocs] = avi
+    .trim()
+    .split(lineSeparator)
+    .map((line) => line.trim().split(columnSeparator))
+    .map(([date, price, , , e, , , , i]) => ({ originalDate: date, date: parseDate(date), price: parseFloat(price), e, i }));
+
+  const groups = [{ date: firstDoc.originalDate, price: firstDoc.price, e: firstDoc.e, i: firstDoc.i }];
+  let lastDate = firstDoc.date;
+
+  for (const { date, price, originalDate, e, i } of restDocs) {
+    // console.log(date, lastDate)
+    if (date - lastDate < ONE_MINUTES) {
+      groups[groups.length - 1].price += price;
+    } else {
+      lastDate = date;
+      groups.push({ date: originalDate, price, e, i });
+    }
+  }
+
+  const transformedGroups = groups.map((group) => Object.values(group).join(columnSeparator)).join(lineSeparator);
+  return transformedGroups;
+}
+
+function getHTML5FileContent(fileHandle) {
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.addEventListener('load', (event) => {
+      resolve(event.target.result);
+    });
+    reader.readAsText(fileHandle);
+  });
+}
+
+function parseDate(date) {
+  return new Date(date.replace(/^(\d\d)\/(\d\d)/, '$2/$1'));
+  // const [day, month, ...rest] = date.split('/');
+  // return  new Date([month, day, ...rest].join('/'));
+}
+
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
